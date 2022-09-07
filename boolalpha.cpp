86d30ae84e57028868512b77b17908a98b270a24
Pedro Saito <pedrosaito@MacBook-Air-de-Pedro.local>
2022-09-07 14:22:56 -0300

    first_kulemba_commit

---
+++ boolalpha.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::noboolalpha;
+using std::boolalpha;
+
+int main(){
+ int num1 {};
+  if(num1 == 0){
+   cout << (num1 == 0) << endl;
+   cout << boolalpha;
+  }
+  
+ return 0;
+}
--- hiwrld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc,char *argv[]){
- string var = "ERROR";
- clog << var << endl;
-
- return 0;
-}
